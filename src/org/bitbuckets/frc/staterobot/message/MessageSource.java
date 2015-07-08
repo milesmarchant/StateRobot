7e01dd9f1443dbacf64e865711216eff05be82d5
Miles <miles@marchant.net>
2015-07-08 06:09:08 -0700

    Messages can be sent via a button on SmartDashboard

---
+++ src/org/bitbuckets/frc/staterobot/message/MessageSource.java
@@ -0,0 +1,13 @@
+package org.bitbuckets.frc.staterobot.message;
+
+public abstract class MessageSource {
+	
+	Messageable target;
+	
+	public MessageSource(Messageable target){
+		this.target = target;
+	}
+	
+	
+
+}
---
+++ src/org/bitbuckets/frc/staterobot/message/SendableMessageSource.java
@@ -0,0 +1,59 @@
+package org.bitbuckets.frc.staterobot.message;
+
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.tables.ITable;
+
+public class SendableMessageSource extends MessageSource implements Sendable{
+	
+	Message message;
+	
+	public SendableMessageSource(Messageable target, Message message, String key) {
+		super(target);
+		this.message = message;
+		SmartDashboard.putData(key, new MessageSenderCommand());
+	}
+
+	@Override
+	public void initTable(ITable subtable) {
+		
+	}
+
+	@Override
+	public ITable getTable() {
+		return null;
+	}
+
+	@Override
+	public String getSmartDashboardType() {
+		return null;
+	}
+	
+	class MessageSenderCommand extends Command{
+
+		@Override
+		protected void initialize() {
+			target.writeMessage(message);
+		}
+
+		@Override
+		protected void execute() {
+		}
+
+		@Override
+		protected boolean isFinished() {
+			return true;
+		}
+
+		@Override
+		protected void end() {
+		}
+
+		@Override
+		protected void interrupted() {
+		}
+		
+	}
+
+}
